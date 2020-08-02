bcc4fe1de19c9ed02bafb38425675e017d1e9bcb
Steven Yao <51345129+skyao2002@users.noreply.github.com>
2020-08-02 15:14:21 -0500

    Updates

---
+++ precache-manifest.b1cfc8eaef6f26f2c04cd5ed12b5023b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8816466dd47c581137fffa05b919619",
+    "url": "/WordHuntSolverFrontend/index.html"
+  },
+  {
+    "revision": "bf477c41def1b6cdd191",
+    "url": "/WordHuntSolverFrontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2bb543c0041b257eabbf",
+    "url": "/WordHuntSolverFrontend/static/css/main.fb113f3b.chunk.css"
+  },
+  {
+    "revision": "bf477c41def1b6cdd191",
+    "url": "/WordHuntSolverFrontend/static/js/2.059f0be7.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/WordHuntSolverFrontend/static/js/2.059f0be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb543c0041b257eabbf",
+    "url": "/WordHuntSolverFrontend/static/js/main.64bbe96a.chunk.js"
+  },
+  {
+    "revision": "7011f5b7cf3e32a2569e",
+    "url": "/WordHuntSolverFrontend/static/js/runtime-main.80f3c344.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/WordHuntSolverFrontend/precache-manifest.b1cfc8eaef6f26f2c04cd5ed12b5023b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WordHuntSolverFrontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
